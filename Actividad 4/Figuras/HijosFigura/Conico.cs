61198b0d0684de32b18da203f7345bd93ade1572
jorda <jorda@JORDAN>
2025-09-15 20:12:43 -0300

    Agregar archivos de proyecto.

---
+++ Actividad 4/Figuras/HijosFigura/Conico.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class Conico : Figura {
+    private decimal _radio;
+
+}
+// ---------------- Heredan de Conico ------------------------
+public class Circunsferencia : Conico
+{
+    public override decimal CalcularPerimetro() {
+        decimal numero = 0;
+        return numero;
+    }
+    public override decimal CalcularArea()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+
+    private decimal _diametro;
+    public decimal ObtenerDiametro()
+    {
+        decimal diametro = 0;
+        this._diametro = diametro;
+        return diametro;
+    }
+}
+public class Elipse : Conico
+{
+
+    private decimal _radioMayor;
+    public override decimal CalcularPerimetro()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+    public override decimal CalcularArea()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+}
+
---
+++ Actividad 4/Figuras/HijosFigura/Poligono.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public abstract class Poligono : Figura
+{
+    private decimal _ladoUno = 0;
+    private int _cantLados = 0;
+
+}
+
+// ------------- Heredan de Poligono -------------------
+public class Cuadrado : Poligono{
+    public override decimal CalcularPerimetro()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+    public override decimal CalcularArea()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+}
+public class Rectangulo : Poligono{
+    private decimal _lado2;
+    public override decimal CalcularPerimetro()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+    public override decimal CalcularArea()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+}
+
+// ----------------------------------------------
+
+ public abstract class Triangulo : Poligono
+{
+
+    private decimal Base;
+    private decimal Altura;
+    public override decimal CalcularPerimetro()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+    public override decimal CalcularArea()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+}
+// --------------- Heredan de Triangulo -----------------------
+public class Equilatero : Triangulo {
+    public override decimal CalcularPerimetro()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+    public override decimal CalcularArea()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+}
+public class Isosceles : Triangulo{ 
+    private decimal _lado2;
+    public override decimal CalcularPerimetro()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+    public override decimal CalcularArea()
+    {
+        decimal numero = 0;
+        return numero;
+    }
+}
